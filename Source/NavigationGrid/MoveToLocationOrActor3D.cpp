03846ef0f3a6b19960c29b30006017bfe98e5540
Nyntex <everminer469@gmail.com>
2024-10-30 17:24:51 +0100

    Latent Action
    Started with a latent action for moving because this can have multiple inputs. While it does have the drawback of not proceeding when it fails I do still think it might be a better approach. I will keep both versions, the Async Action and the Latent action and will make sure they are compatible later on
    Duplicated the showcase level so I have one with many units and one with only one unit, this way I don't have to remove them

--- Source/NavigationGrid/MoveToLocationOrActor3D.cpp
+++ Source/NavigationGrid/MoveToLocationOrActor3D.cpp
@@ -3,6 +3,23 @@
 
 #include "MoveToLocationOrActor3D.h"
 
+#include "VectorTypes.h"
+
+void UMoveToActorOrLocation3D::Stop3DMovement(APawn* WorldContext)
+{
+	if (!WorldContext) return;
+
+	auto it = MoveToLocationOrActor3DStatics::CurrentMovingPawns.find(WorldContext);
+	if (it != MoveToLocationOrActor3DStatics::CurrentMovingPawns.end())
+	{
+		it->second->WantToCancel = true;
+		MoveToLocationOrActor3DStatics::CurrentMovingPawns.erase(it);
+		Stop3DMovement(WorldContext);
+	}
+
+}
+
+#pragma region AsyncAction
 UMoveToLocationOrActor3D* UMoveToLocationOrActor3D::MoveToLocationOrActor3D(APawn* WorldContext, FVector Location)
 {
 	UMoveToLocationOrActor3D* Action = NewObject<UMoveToLocationOrActor3D>();
@@ -43,13 +60,13 @@ void UMoveToLocationOrActor3D::Activate()
 
 void UMoveToLocationOrActor3D::CancelMovement()
 {
-
 	OnCanceled.Broadcast();
 	FinishMovement();
 }
 
 void UMoveToLocationOrActor3D::Tick(float DeltaTime)
 {
+	if (!MovingTarget || !World) return;
 	UpdateMove(DeltaTime);
 	OnMove.Broadcast();
 }
@@ -61,8 +78,123 @@ void UMoveToLocationOrActor3D::UpdateMove(float DeltaTime)
 
 void UMoveToLocationOrActor3D::FinishMovement()
 {
-	//MoveTicker.UnRegisterTickFunction();
 	MoveToLocationOrActor3DStatics::CurrentMovingObjects.erase(MovingTarget);
-
+	MovingTarget = nullptr;
+	World = nullptr;
 	SetReadyToDestroy();
 }
+#pragma endregion
+
+#pragma region LatentAction
+void UMoveToActorOrLocation3D::MoveToActorOrLocation3D(APawn* WorldContext, FLatentActionInfo LatentInfo,
+	EMoveInputPins InputPins, EMoveOutputPins& OutputPins, FVector MoveLocation, FVector& CurrentMoveDirection)
+{
+	UWorld* World = GEngine->GetWorldFromContextObject(WorldContext, EGetWorldErrorMode::ReturnNull);
+
+#if WITH_EDITOR
+	if(World == nullptr && GEditor != nullptr)
+	{
+		World = GEditor->GetEditorWorldContext().World();
+	}
+#endif
+
+	if(World == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Latent Move To Actor or Location Failed - No world available!"));
+		return;
+	}
+	FLatentActionManager& LatentActionManager = World->GetLatentActionManager();
+
+	FLatentMoveToActorOrLocation3D* ExistingAction = LatentActionManager.FindExistingAction<FLatentMoveToActorOrLocation3D>(LatentInfo.CallbackTarget, LatentInfo.UUID);
+	
+	switch (InputPins) {
+	case EMoveInputPins::Start:
+	{
+		//Cancel any existing action
+		Stop3DMovement(WorldContext);
+
+		FLatentMoveToActorOrLocation3D* Action;
+
+		if (!ExistingAction)
+		{
+			//Even though this instance is getting created with new, I do not have to worry about deleting it, Unreal does it for me
+			Action = new FLatentMoveToActorOrLocation3D(LatentInfo, OutputPins, WorldContext, MoveLocation, CurrentMoveDirection);
+			LatentActionManager.AddNewAction(LatentInfo.CallbackTarget, LatentInfo.UUID, Action);
+		}
+		else
+		{
+			ExistingAction->WantToCancel = true;
+			Action = new FLatentMoveToActorOrLocation3D(LatentInfo, OutputPins, WorldContext, MoveLocation, CurrentMoveDirection);
+			LatentActionManager.AddNewAction(LatentInfo.CallbackTarget, LatentInfo.UUID, Action);
+		}
+
+			//MoveToLocationOrActor3DStatics::CurrentMovingPawns.try_emplace(WorldContext, Action);
+		MoveToLocationOrActor3DStatics::CurrentMovingPawns.insert({ WorldContext, Action });
+		
+	}
+	case EMoveInputPins::Cancel:
+	{
+		if(ExistingAction)
+		{
+			ExistingAction->WantToCancel = true;
+
+			auto it = MoveToLocationOrActor3DStatics::CurrentMovingPawns.find(WorldContext);
+			if (it != MoveToLocationOrActor3DStatics::CurrentMovingPawns.end())
+			{
+				MoveToLocationOrActor3DStatics::CurrentMovingPawns.erase(it);
+			}
+		}
+	}
+	}
+}
+
+#define ResponseLatentInfo LatentActionInfo.ExecutionFunction, LatentActionInfo.Linkage, LatentActionInfo.CallbackTarget
+
+void FLatentMoveToActorOrLocation3D::UpdateOperation(FLatentResponse& Response)
+{
+	//FPendingLatentAction::UpdateOperation(Response);
+
+	if(WantToCancel)
+	{
+		//MoveToLocationOrActor3DStatics::CurrentMovingPawns.erase(MovementTarget);
+		Output = EMoveOutputPins::OnCanceled;
+		Response.FinishAndTriggerIf(true, ResponseLatentInfo);
+		return;
+	}
+
+	float DeltaTime = Response.ElapsedTime();
+
+	if(IsFirstCall)
+	{
+		GetNewPath();
+		IsFirstCall = false;
+		Output = EMoveOutputPins::OnStarted;
+		Response.TriggerLink(ResponseLatentInfo);
+		return;
+	}
+
+	if(UE::Geometry::Distance(MoveLocation, MovementTarget->GetActorLocation()) <= 50) //Done moving?
+	{
+		MoveToLocationOrActor3DStatics::CurrentMovingPawns.erase(MovementTarget);
+		Output = EMoveOutputPins::OnCompleted;
+		Response.FinishAndTriggerIf(true, ResponseLatentInfo);
+		return;
+	}
+
+	//Standard Move behavior
+	UpdateMovement(DeltaTime);
+	Output = EMoveOutputPins::OnMove;
+	Response.TriggerLink(ResponseLatentInfo);
+}
+
+void FLatentMoveToActorOrLocation3D::GetNewPath()
+{
+
+}
+
+void FLatentMoveToActorOrLocation3D::UpdateMovement(float DeltaTime)
+{
+
+}
+
+#pragma endregion
--- Source/NavigationGrid/MoveToLocationOrActor3D.h
+++ Source/NavigationGrid/MoveToLocationOrActor3D.h
@@ -5,10 +5,12 @@
 #include "CoreMinimal.h"
 #include <map>
 #include "Delegates/DelegateCombinations.h"
+#include "LatentActions.h"
 #include "Kismet/BlueprintAsyncActionBase.h"
 #include "MoveToLocationOrActor3D.generated.h"
 
 
+#pragma region AsyncAction
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FMoveToLocationOrActor3DOutputPin);
 
 UCLASS(BlueprintType, meta=(ExposedAsyncProxy = AsyncAction))
@@ -21,16 +23,16 @@ public:
 	static UMoveToLocationOrActor3D* MoveToLocationOrActor3D(APawn* WorldContext, FVector Location);
 
 	//Should fire every tick right after the move command has been given
-	UPROPERTY(BlueprintReadOnly, Category= "Move to Location or Actor 3D")
+	UPROPERTY(BlueprintReadOnly, Category= "Move to Location or Actor 3D", BlueprintAssignable)
 	FMoveToLocationOrActor3DOutputPin OnMove;
 
-	UPROPERTY(BlueprintReadOnly, Category = "Move to Location or Actor 3D")
+	UPROPERTY(BlueprintReadOnly, Category = "Move to Location or Actor 3D", BlueprintAssignable)
 	FMoveToLocationOrActor3DOutputPin OnFinish;
 
-	UPROPERTY(BlueprintReadOnly, Category = "Move to Location or Actor 3D")
+	UPROPERTY(BlueprintReadOnly, Category = "Move to Location or Actor 3D", BlueprintAssignable)
 	FMoveToLocationOrActor3DOutputPin OnCanceled;
 
-	UPROPERTY(BlueprintReadOnly, Category = "Move to Location or Actor 3D")
+	UPROPERTY(BlueprintReadOnly, Category = "Move to Location or Actor 3D", BlueprintAssignable)
 	FMoveToLocationOrActor3DOutputPin OnFailed;
 
 private:
@@ -75,10 +77,117 @@ private:
 	UPROPERTY()
 	TObjectPtr<UWorld> World;
 };
+#pragma endregion
+
+
+#pragma region LatentAction
+
+UENUM()
+enum class EMoveInputPins : uint8
+{
+	Start,		//Starts movement
+	Cancel,		//Cancels movement
+};
+
+UENUM()
+enum class EMoveOutputPins : uint8
+{
+	OnStarted,		//Called only once on start
+	OnMove,			//Every Frame
+	OnCanceled,		//When node is canceled
+	OnCompleted,	//when the movement is completed
+	OnFailed,		//When movement failed
+};
+
+UCLASS()
+class UMoveToActorOrLocation3D : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	/**
+	 *Start the movement and move context object to target location.
+	 *
+	 *Fails if location is outside of 3D Navigation Grid or location is invalid
+	 *
+	 *Only one instance can be created per object. When creating a new latent action the old one will be canceled
+	 *
+	 *@param WorldContext			Variable to hold the object calling this function. Pin will be hidden
+	 *@param LatentInfo				Variable to hold the information required to process a latent function. Will be hidden on the node
+	 *@param InputPins				Variable to hold the different input pins
+	 *@param OutputPins				Variable to hold the different output pins
+	 *@param MoveLocation			Variable to hold the location the context object is supposed to be moved to
+	 *@param CurrentMoveDirection	Gives the current direction the context is moving towards, this is a local value and does not show the final location
+	 */
+	UFUNCTION(BlueprintCallable, meta=(WorldContext = "WorldContext", Latent, LatentInfo = "LatentInfo", 
+		ExpandEnumAsExecs = "InputPins,OutputPins"), Category = "Move to Actor or Location 3D")
+	static void MoveToActorOrLocation3D(APawn* WorldContext, FLatentActionInfo LatentInfo, EMoveInputPins InputPins, 
+		EMoveOutputPins& OutputPins, FVector MoveLocation, FVector& CurrentMoveDirection);
+
+	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContext"))
+	static void Stop3DMovement(APawn* WorldContext);
+};
+
+
+class FLatentMoveToActorOrLocation3D : public FPendingLatentAction
+{
+public:
+	TObjectPtr<APawn> MovementTarget;
+
+	FVector MoveLocation = FVector::Zero();
+
+public:
+	FVector& CurrentMoveDirection;
+
+	bool IsFirstCall = true;
+
+	bool WantToCancel = false;
+
+public:
+	FLatentActionInfo LatentActionInfo;
+
+	EMoveOutputPins& Output;
+
+	FLatentMoveToActorOrLocation3D(FLatentActionInfo& LatentInfo, EMoveOutputPins& OutputPins, APawn* WorldContext,
+		FVector MoveLocation, FVector& CurrentMoveDirection)
+			: MovementTarget(WorldContext), MoveLocation(MoveLocation), CurrentMoveDirection(CurrentMoveDirection), LatentActionInfo(LatentInfo), Output(OutputPins)
+	{
+		Output = EMoveOutputPins::OnStarted;
+		IsFirstCall = true;
+		CurrentMoveDirection = FVector::Zero();
+	}
+
+	virtual void UpdateOperation(FLatentResponse& Response) override;
+
+	void GetNewPath();
+
+	void UpdateMovement(float DeltaTime);
+
+#if WITH_EDITOR
+	virtual FString GetDescription() const override
+	{
+		FString text = MovementTarget.GetName() + TEXT(" is moving from ") + MovementTarget->GetActorLocation().ToString() + TEXT(" to ") + MoveLocation.ToString() + TEXT("\n The current direction is ") + CurrentMoveDirection.ToString();
+		return text;
+	}
+#endif
+
+protected:
+	TArray<FVector> Path = TArray<FVector>();
+
+	FVector LastPositionToMoveTo = FVector::Zero();
+
+	float ClosenessThreshold = 0;
+
+};
+#pragma endregion
+
 
 namespace MoveToLocationOrActor3DStatics
 {
 	//This Array will limit the amount of actions to one per object so that the same
 	//Action will not get called on the same target twice.
 	static std::map<APawn*, UMoveToLocationOrActor3D*> CurrentMovingObjects = std::map<APawn*, UMoveToLocationOrActor3D*>();
+
+	static std::map<APawn*, FLatentMoveToActorOrLocation3D*>  CurrentMovingPawns = std::map<APawn*, FLatentMoveToActorOrLocation3D*>();
 }
